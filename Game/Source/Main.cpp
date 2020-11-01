96049006d48319873b9e23e3bc738a17871d7cb5
Chuchocoronel <carlosmegia1@gmail.com>
2020-11-01 19:03:38 +0100

    New F3 error fixed and hide console added

--- Game/Source/Main.cpp
+++ Game/Source/Main.cpp
@@ -30,7 +30,7 @@ int main(int argc, char* args[])
 {
 	LOG("Engine starting ...");
 
-	ShowWindow(GetConsoleWindow(), SW_SHOW);
+	ShowWindow(GetConsoleWindow(), SW_HIDE);
 
 
 	MainState state = CREATE;
